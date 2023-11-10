1281eec855ebbb96fe4e14fad4ed2d709605a6b0
Hepheir <hepheir@gmail.com>
2023-11-10 23:45:05 +0900

    feat(install.sh): 잘못된 tar 파일을 받았을 경우에 대한 예외처리

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # 위에 있는 링크에서 설문을 마치면 주는 다운로드 파일의 url 주소로 바꿔주어야 합니다.
 # =============================================================================
 
-download_url="https://developer.download.nvidia.com/compute/cudnn/secure/8.9.6/local_installers/11.x/cudnn-linux-x86_64-8.9.6.50_cuda11-archive.tar.xz?..."
+download_url="https://developer.download.nvidia.com/compute/cudnn/..."
 
 
 # =============================================================================
@@ -13,14 +13,22 @@ download_url="https://developer.download.nvidia.com/compute/cudnn/secure/8.9.6/l
 # 본 스크립트는 다음의 가이드에 따라 작업을 수행합니다.
 # https://docs.nvidia.com/deeplearning/cudnn/install-guide/index.html
 
-download_dirname="cudnn"
+download_dirname="./cudnn"
 download_filename="$download_dirname.tar.xz"
 
 wget -O $download_filename $download_url
 
 # 1.3. Installing on Linux
 # 1.3.1. Tar File Installation
-tar -xvf $download_at
+tar -xvf $download_filename
+
+if [ "$?" -ne 0 ]; then
+    echo "============================================================================="
+    echo " 다운 받은 파일이 TAR 압축 파일이 아닐 수도 있습니다."
+    echo " $download_filename 을 확인해보십시오."
+    echo "============================================================================="
+    exit 1
+fi
 
 cd $download_dirname
 
